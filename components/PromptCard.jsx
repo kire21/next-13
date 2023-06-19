4d0cfd1a48ab02fb56c018c07a2cabbce51aeeb1
kire21 <kire.zdraveski@gmail.com>
2023-06-19 17:04:01 +0200

    add maring bottom on card prompt

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -18,7 +18,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   };
 
   return (
-    <div className="prompt_card">
+    <div className="prompt_card mb-5">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
